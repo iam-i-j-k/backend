31e1591c32a953c34309f5e57541606fdae0d131
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-02-28 08:47:18 +0530

    cors options

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const io = new Server(server, {
     credentials: true
   }
 });
-
+app.options("*", cors()); // Handle preflight requests for all routes
 // Active users storage
 const users = new Map();
 
