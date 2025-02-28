8e7c37a759b9505895c85793eec331c4fdcada0c
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-02-28 08:51:17 +0530

    cors origin updated

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.NODE_ENV === 'production' 
       ? process.env.FRONTEND_URL 
-      : "http://localhost:5173",
+      : "https://skillswap2.vercel.app",
     methods: ["GET", "POST"],
     credentials: true
   }
