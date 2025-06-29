2c16652059afaa3ffa5e16b4a1d0ddc9753085ad
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-06-29 23:22:49 +0530

    config

--- config.js
+++ config.js
@@ -2,14 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
+  environment: process.env.NODE_ENV || 'development',
+  corsOrigin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  redis: {
+    url: process.env.REDIS_URL
+  },
   port: process.env.PORT || 8000,
   mongoUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
   frontendUrl: process.env.FRONTEND_URL,
-  redis: {
-    url: process.env.REDIS_URL || `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:12960`
-  },
-  corsOrigin: process.env.FRONTEND_URL || 'http://localhost:5173',
 };
 
 export default config;
