87c6de9be232d0c6336f033620df1b7401a71ebe
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-06-29 21:33:15 +0530

    ignore env

--- config.js
+++ config.js
@@ -9,10 +9,7 @@ const config = {
   redis: {
     url: process.env.REDIS_URL || `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:12960`
   },
-  corsOrigin: process.env.NODE_ENV === "production" 
-    ? process.env.FRONTEND_URL
-    : ["http://localhost:5173"],
-  environment: process.env.NODE_ENV || 'development'
+  corsOrigin: process.env.FRONTEND_URL || 'http://localhost:5173',
 };
 
 export default config;
