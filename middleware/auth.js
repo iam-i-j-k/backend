d2f526bf4568f7950a75ea03ee676d0a87808aab
Irfan <irfanjankhan7860@gmail.com>
2025-07-10 11:46:22 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,6 @@ export const auth = async (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.status(401).json({ error: 'Authentication required' });
 
-  console.log(token)
   try {
     const decoded = jwt.verify(token, config.jwtSecret);
     req.user = decoded;
